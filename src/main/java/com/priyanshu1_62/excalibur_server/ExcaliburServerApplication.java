f8335a98124e8e732f93198a53fbfdb8b36aba96
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2026-05-19 02:12:03 +0530

    Initialize Spring Boot server with PostgreSQL and Spring Security

---
+++ src/main/java/com/priyanshu1_62/excalibur_server/ExcaliburServerApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshu1_62.excalibur_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcaliburServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcaliburServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshu1_62/excalibur_server/ExcaliburServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu1_62.excalibur_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcaliburServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
